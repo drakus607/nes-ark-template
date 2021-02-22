ff06f4e227bbeada53f3294101f603ed2cfdaae6
nanttomera <nanttomera@gmail.com>
2021-02-22 21:51:14 +0100

    @logo font size adjusted

--- src/Components/Navbar/Logo.js
+++ src/Components/Navbar/Logo.js
@@ -12,17 +12,17 @@ const StyledLogo = styled.h1`
 padding: 0;
 text-decoration: none;
 font-size: 1.2rem;
-margin-left: 1rem;
+margin-left: 2rem;
 line-height: 1;
 font-weight: 600;
 letter-spacing: 0.8px;
 font-family: "Poppins", sans-serif;
 @media(min-width: 420px){
     font-size: 1.5rem;
-    margin-left: 3rem;
 }
 @media(min-width: 530px){
     font-size: 2rem;
+    margin-left: 3rem;
 }
 @media(min-width: 1100px){
     padding-right: 2rem;
